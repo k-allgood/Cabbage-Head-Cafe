3a4a545435942166ddb6aa58c222ab6967855076
k-allgood <48075080+k-allgood@users.noreply.github.com>
2019-05-24 22:10:36 -0400

    Add files via upload

---
+++ scripts/hamburger.js
@@ -0,0 +1,13 @@
+//JS for hamburger menu on Sweet Kicks
+function onClickMenu() {
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("navigation").classList.toggle("change");
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+const menu = document.querySelector("#menu");
+menu.addEventListener("click", function() {
+	onClickMenu();
+});
+
+
