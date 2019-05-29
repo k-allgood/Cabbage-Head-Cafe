60de19e1cba72b7b5ac2031d7360b6d63bfa19e0
k-allgood <48075080+k-allgood@users.noreply.github.com>
2019-05-29 17:59:59 -0400

    Add files via upload

--- scripts/events.js
+++ scripts/events.js
@@ -1,9 +1,1 @@
-window.onload = function () { 
-	var today = new Date();
-	var day = today.getDate();
-	var month = today.getMonth()+1;
-	var year = today.getFullYear();
-
-	today = month + "." + day + "." + year;
-	document.getElementById("currentDate").innerHTML = "Today is " + today;
-}
+window.onload=function(){var today=new Date();var day=today.getDate();var month=today.getMonth()+1;var year=today.getFullYear();today=month+"."+day+"."+year;document.getElementById("currentDate").innerHTML="Today is "+today}
